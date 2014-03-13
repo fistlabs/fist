ff664a19a47f187a7d338b9bb470f6abc39dce8d
spacy <spacy@yandex-team.ru>
2014-03-13 04:25:07 +0400

    v3.0.x: remove fist.util.namespace dependence

--- bundle/Nested.js
+++ bundle/Nested.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var RE_WHITESPACE = /^\s+$/;
+
 var Bundle = /** @type Bundle */ require('./Bundle');
-var Namespace = /** @type Namespace */ require('fist.util.namespace/Namespace');
 
 var extend = require('fist.lang.extend');
+var cache = Object.create(null);
+var hasProperty = Object.prototype.hasOwnProperty;
 
 /**
  * @class Nested
@@ -22,7 +25,7 @@ var Nested = Bundle.extend(/** @lends Nested.prototype */ {
      * */
     _link: function (root, path, data) {
 
-        var nmsp = Namespace.useOn(root, path);
+        var nmsp = Nested.use(root, path);
 
         if ( Object(nmsp) === nmsp ) {
             extend(nmsp, data);
@@ -30,9 +33,189 @@ var Nested = Bundle.extend(/** @lends Nested.prototype */ {
             return;
         }
 
-        Namespace.linkOn(root, path, data);
+        Nested.link(root, path, data);
     }
 
+}, {
+
+    /**
+     * @public
+     * @static
+     * @memberOf Nested
+     *
+     * @param {String} path
+     *
+     * @returns {?}
+     *
+     * @throws {SyntaxError}
+     * */
+    parse: function (path) {
+        /*eslint complexity: [2, 14] */
+        var cursor;
+        var i;
+        var isEscaped;
+        var isPart;
+        var l;
+        var part;
+        var parts;
+
+        if ( path in cache ) {
+
+            return cache[path];
+        }
+
+        isEscaped = 0;
+        isPart = 1;
+        part = '';
+        parts = [];
+
+        for ( i = 0, l = path.length; i < l; i += 1 ) {
+            cursor = path.charAt(i);
+
+            if ( '\\' === cursor && 0 === isEscaped ) {
+                isEscaped = 1;
+
+                continue;
+            }
+
+            if ( 1 === isEscaped ) {
+
+                if ( 0 === isPart ) {
+
+                    throw new SyntaxError(path);
+                }
+
+                part += cursor;
+                isEscaped = 0;
+
+                continue;
+            }
+
+            if ( 0 === isPart ) {
+
+                if ( RE_WHITESPACE.test(cursor) ) {
+
+                    continue;
+                }
+
+                if ( '.' === cursor ) {
+                    part = '';
+                    isPart = 1;
+
+                    continue;
+                }
+
+                throw new SyntaxError(path);
+            }
+
+            if ( RE_WHITESPACE.test(cursor) ) {
+
+                if ( 0 < part.length ) {
+                    parts[parts.length] = part;
+                    isPart = 0;
+                }
+
+                continue;
+            }
+
+            if ( '.' === cursor ) {
+                parts[parts.length] = part;
+                isPart = 0;
+                i -= 1;
+
+                continue;
+            }
+
+            part += cursor;
+        }
+
+        if ( 1 === isEscaped ) {
+
+            throw new SyntaxError(path);
+        }
+
+        if ( 1 === isPart ) {
+            parts[parts.length] = part;
+        }
+
+        cache[path] = parts;
+
+        return parts;
+    },
+
+    /**
+     * @public
+     * @static
+     * @memberOf Nested
+     *
+     * @method
+     *
+     * @param {Object} root
+     * @param {String} path
+     * @param {*} data
+     *
+     * @returns {*}
+     * */
+    link: function (root, path, data) {
+
+        var i;
+        var l;
+        var part;
+        var parts = Nested.parse(path);
+
+        for ( i = 0, l = parts.length - 1; i < l; i += 1 ) {
+            part = parts[i];
+
+            if ( hasProperty.call(root, part) ) {
+
+                if ( Object(root[part]) !== root[part] ) {
+                    root[part] = Object.create(null);
+                }
+
+            } else {
+                root[part] = Object.create(null);
+            }
+
+            root = root[part];
+        }
+
+        part = parts[l];
+        root[part] = data;
+
+        return root[part];
+    },
+
+    /**
+     * @public
+     * @static
+     * @memberOf Nested
+     *
+     * @method
+     *
+     * @param {Object} root
+     * @param {String} path
+     *
+     * @returns {*}
+     * */
+    use: function (root, path) {
+
+        var i;
+        var l;
+        var parts = Nested.parse(path);
+
+        for ( i = 0, l = parts.length; i < l; i += 1 ) {
+
+            if ( Object(root) === root ) {
+                root = root[parts[i]];
+
+                continue;
+            }
+
+            return void 0;
+        }
+
+        return root;
+    }
 });
 
 module.exports = Nested;
--- test/common/bundle/Nested.js
+++ test/common/bundle/Nested.js
@@ -25,5 +25,87 @@ module.exports = {
         });
 
         test.done();
+    },
+
+    link0: function (test) {
+
+        var root;
+
+        root = {
+            a: {}
+        };
+
+        Nested.link(root, 'a.b.c', 5);
+
+        test.deepEqual(root, {a: {b: {c: 5}}});
+
+        root = {
+            a: 0
+        };
+
+        Nested.link(root, 'a.b.c', 5);
+
+        test.deepEqual(root, {a: {b: {c: 5}}});
+
+        test.done();
+    },
+
+    'Strings should be parsed correctly': function (test) {
+        test.deepEqual( Nested.parse('a.b.c'), ['a', 'b', 'c'] );
+        test.done();
+    },
+
+    'Escaping should be supported': function (test) {
+        test.deepEqual( Nested.parse('a.b\\.c'), ['a', 'b.c'] );
+        test.deepEqual( Nested.parse('a.b\\ \\.c'), ['a', 'b .c'] );
+        test.deepEqual( Nested.parse('a.b.\\ .c'), ['a', 'b', ' ', 'c'] );
+        test.deepEqual( Nested.parse('a.b.\\ .c'), ['a', 'b', ' ', 'c'] );
+        test.done();
+    },
+
+    'Partitions can not be contain whitespaces': function (test) {
+
+        try {
+            Nested.parse('a.b c');
+            throw 0;
+        } catch (ex) {
+            test.ok( ex instanceof SyntaxError );
+        }
+
+        try {
+            Nested.parse('a.b \\c');
+            throw 0;
+        } catch (ex) {
+            test.ok( ex instanceof SyntaxError );
+        }
+
+        test.done();
+    },
+
+    'Partition separators can be wrapped by whitespaces': function (test) {
+
+        test.deepEqual( Nested.parse(' a.b.c '), ['a', 'b', 'c']);
+        test.deepEqual( Nested.parse(' a. b . c '), ['a', 'b', 'c']);
+        test.deepEqual( Nested.parse(' a   .b . c'), ['a', 'b', 'c']);
+
+        test.done();
+    },
+
+    'Escape character can not be placed at end of partition': function (test) {
+
+        try {
+            Nested.parse('a.c.b\\');
+            throw 0;
+        } catch (ex) {
+            test.ok( ex instanceof SyntaxError );
+        }
+
+        test.done();
+    },
+
+    'Should return an object that specified by pattern': function (test) {
+        test.strictEqual( Nested.use({a:{b: {c: 0}}}, 'a.b.c'), 0 ) ;
+        test.done();
     }
+
 };
