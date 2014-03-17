5f8fa788c3d5c5c7e92c91d9c34b6119175f9edd
golyshevd <golyshev.dmitry@yandex.ru>
2014-03-18 01:14:14 +0400

    rc-dev: refactor track/Runtime tests

--- test/common/track/Runtime.js
+++ test/common/track/Runtime.js
@@ -8,65 +8,69 @@ var sock = require('../../stuff/conf/sock');
 
 module.exports = {
 
-    arg: function (test) {
+    'Runtime.prototype.arg': [
+        function (test) {
 
-        var fist = new Fist();
+            var fist = new Fist();
 
-        fist.route('GET', '/<page=about>/(<sub>)', 'index');
+            fist.route('GET', '/<page=about>/(<sub>)', 'index');
 
-        fist.decl('index', function (track) {
-            test.strictEqual(track.arg('page', true), 'about');
-            test.strictEqual(track.arg('sub'), '80');
-            track.send(200);
-        });
+            fist.decl('index', function (track) {
+                test.strictEqual(track.arg('page', true), 'about');
+                test.strictEqual(track.arg('sub'), '80');
+                track.send(200);
+            });
 
-        try {
-            Fs.unlinkSync(sock);
-        } catch (ex) {}
+            try {
+                Fs.unlinkSync(sock);
+            } catch (ex) {}
 
-        fist.listen(sock);
+            fist.listen(sock);
 
-        asker({
-            method: 'GET',
-            socketPath: sock,
-            path: '/about/?page=index&sub=80'
-        }, function (err, data) {
-            test.ok(data);
-            test.strictEqual(data.statusCode, 200);
-            test.done();
-        });
-    },
+            asker({
+                method: 'GET',
+                socketPath: sock,
+                path: '/about/?page=index&sub=80'
+            }, function (err, data) {
+                test.ok(data);
+                test.strictEqual(data.statusCode, 200);
+                test.done();
+            });
+        }
+    ],
 
-    buildPath: function (test) {
+    'Runtime.prototype.buildPath': [
+        function (test) {
 
-        var fist = new Fist();
+            var fist = new Fist();
 
-        fist.route('GET', '/(<pageName>/)', 'url');
+            fist.route('GET', '/(<pageName>/)', 'url');
 
-        fist.decl('url', function (track, errors, result, done) {
-            done(null, track.buildPath('url', {
-                pageName: 'about',
-                text: 'test'
-            }));
-        });
+            fist.decl('url', function (track, errors, result, done) {
+                done(null, track.buildPath('url', {
+                    pageName: 'about',
+                    text: 'test'
+                }));
+            });
 
-        try {
-            Fs.unlinkSync(sock);
-        } catch (ex) {}
+            try {
+                Fs.unlinkSync(sock);
+            } catch (ex) {}
 
-        fist.listen(sock);
+            fist.listen(sock);
 
-        asker({
-            method: 'GET',
-            socketPath: sock,
-            path: '/'
-        }, function (err, data) {
-            test.strictEqual(data.data + '', '/about/?text=test');
-            test.done();
-        });
-    },
+            asker({
+                method: 'GET',
+                socketPath: sock,
+                path: '/'
+            }, function (err, data) {
+                test.strictEqual(data.data + '', '/about/?text=test');
+                test.done();
+            });
+        }
+    ],
 
-    writeError: [
+    'Runtime.prototype.send': [
         function (test) {
 
             var fist = new Fist();
