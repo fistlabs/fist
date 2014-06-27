3245e5672d1826a2aa67ce4b42b2388a120abf97
spacy <spacy@yandex-team.ru>
2014-06-27 17:35:29 +0400

    Сделал возможность унаследовать при инстанцировании При вызове fist() можно теперь передать сразу прототип и статические методы var app = fist(params, members, statics)

--- test/root/fist.js
+++ test/root/fist.js
@@ -9,5 +9,12 @@ module.exports = [
         test.ok(app instanceof Framework);
         test.deepEqual(app.params, {a: 42});
         test.done();
+    },
+    function (test) {
+        var app = fist({a: 42}, {oO: 'test'});
+        test.ok(app instanceof Framework);
+        test.deepEqual(app.params, {a: 42});
+        test.strictEqual(app.oO, 'test');
+        test.done();
     }
 ];
