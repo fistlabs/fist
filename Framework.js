e48788803eb524491aafc8639189ccfa2fb2b99a
golyshevd <golyshev.dmitry@yandex.ru>
2014-07-09 02:08:44 +0400

    Переопределяю базовый узел в классе Framework Теперь очередь в _serial не будет продолжаться если был выполнен ответ, а также не будет закэширован ответ если был выполнен ответ

--- Framework.js
+++ Framework.js
@@ -49,6 +49,16 @@ var Framework = inherit(Tracker, /** @lends Framework.prototype */ {
         this.router = this._createRouter(this.params.router);
 
         this.plug(plugRoutes);
+
+        //  Необходимо переопределить базовый узел
+        // чтобы он мог замечать сайд-эффекты
+        this.unit({
+            path: '_unit',
+            _hasOutsideResolved: function (ctx) {
+
+                return ctx.track.res.hasResponded();
+            }
+        });
     },
 
     /**
