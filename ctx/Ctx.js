f89b6cf9eb99ead0b5723fa873d4bd818f669cae
golyshevd <golyshev.dmitry@yandex.ru>
2014-07-19 01:41:14 +0400

    Перенес API связанное с namespace в отдельный модуль

--- ctx/Ctx.js
+++ ctx/Ctx.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var R_WHITESPACE = /^\s+$/;
-
 var _ = require('lodash-node');
-var cache = Object.create(null);
 var inherit = require('inherit');
+var ns = require('../util/ns');
 var vow = require('vow');
 
 /**
@@ -109,7 +107,7 @@ var Ctx = inherit(/** @lends Ctx.prototype */ {
      * */
     getRes: function (path) {
 
-        return Ctx.use(this.res, path);
+        return ns.use(this.res, path);
     },
 
     /**
@@ -123,7 +121,7 @@ var Ctx = inherit(/** @lends Ctx.prototype */ {
      * */
     getErr: function (path) {
 
-        return Ctx.use(this.ers, path);
+        return ns.use(this.ers, path);
     },
 
     /**
@@ -165,7 +163,7 @@ var Ctx = inherit(/** @lends Ctx.prototype */ {
      * */
     setRes: function (path, data) {
 
-        return Ctx.add(this.res, path, data);
+        return ns.add(this.res, path, data);
     },
 
     /**
@@ -178,7 +176,7 @@ var Ctx = inherit(/** @lends Ctx.prototype */ {
      * */
     setErr: function (path, data) {
 
-        return Ctx.add(this.ers, path, data);
+        return ns.add(this.ers, path, data);
     },
 
     /**
@@ -203,231 +201,6 @@ var Ctx = inherit(/** @lends Ctx.prototype */ {
         return promise;
     }
 
-}, {
-
-    /**
-     * @public
-     * @memberOf Ctx
-     * @method
-     *
-     * @param {Object} root
-     * @param {String} path
-     * @param {*} data
-     * */
-    add: function (root, path, data) {
-
-        var existingData = Ctx.use(root, path);
-
-        if ( _.isObject(existingData) ) {
-
-            return _.extend(existingData, data);
-        }
-
-        return Ctx.link(root, path, data);
-    },
-
-    /**
-     * @public
-     * @static
-     * @memberOf Ctx
-     *
-     * @method
-     *
-     * @param {Object} root
-     * @param {String} path
-     * @param {*} data
-     *
-     * @returns {*}
-     * */
-    link: function (root, path, data) {
-
-        var i;
-        var l;
-        var part;
-        var parts = this.parsePath(path);
-
-        for ( i = 0, l = parts.length - 1; i < l; i += 1 ) {
-            part = parts[i];
-
-            if ( _.has(root, part) ) {
-
-                if ( !_.isObject(root[part]) ) {
-                    root[part] = {};
-                }
-
-            } else {
-                root[part] = {};
-            }
-
-            root = root[part];
-        }
-
-        part = parts[l];
-        root[part] = data;
-
-        return root[part];
-    },
-
-    /**
-     * @public
-     * @static
-     * @memberOf Ctx
-     *
-     * @param {String} path
-     *
-     * @returns {?}
-     *
-     * @throws {SyntaxError}
-     * */
-    parsePath: function (path) {
-
-        if ( path in cache ) {
-
-            return cache[path];
-        }
-
-        cache[path] = parsePath(path);
-
-        return cache[path];
-    },
-
-    /**
-     * @public
-     * @static
-     * @memberOf Ctx
-     *
-     * @method
-     *
-     * @param {Object} root
-     * @param {String} path
-     *
-     * @returns {*}
-     * */
-    use: function (root, path) {
-
-        var i;
-        var l;
-        var parts = this.parsePath(path);
-
-        for ( i = 0, l = parts.length; i < l; i += 1 ) {
-
-            if ( _.isObject(root) ) {
-                root = root[parts[i]];
-
-                continue;
-            }
-
-            return void 0;
-        }
-
-        return root;
-    }
-
 });
-
-/**
- * @private
- * @static
- * @memberOf Ctx
- * @method
- *
- * @param {String} path
- *
- * @returns {Array<String>}
- * */
-function parsePath (path) {
-    /*eslint complexity: [2, 13] */
-    var cur;
-    var index;
-    var isEscape = false;
-    var isChunk = true;
-    var chunk = '';
-    var parts = [];
-
-    /* eslint no-cond-assign: 0 */
-    for ( index = 0; cur = path.charAt(index); index += 1 ) {
-
-        if ( '\\' === cur && !isEscape ) {
-            isEscape = true;
-
-            continue;
-        }
-
-        if ( isEscape ) {
-
-            if ( !isChunk ) {
-
-                throw new SyntaxError(path);
-            }
-
-            chunk += cur;
-            isEscape = false;
-
-            continue;
-        }
-
-        if ( !isChunk ) {
-
-            if ( isSpace(cur) ) {
-
-                continue;
-            }
-
-            if ( '.' === cur ) {
-                chunk = '';
-                isChunk = true;
-
-                continue;
-            }
-
-            throw new SyntaxError(path);
-        }
-
-        if ( isSpace(cur) ) {
-
-            if ( chunk.length ) {
-                parts.push(chunk);
-                isChunk = false;
-            }
-
-            continue;
-        }
-
-        if ( '.' === cur ) {
-            parts.push(chunk);
-            isChunk = false;
-            index -= 1;
-
-            continue;
-        }
-
-        chunk += cur;
-    }
-
-    if ( isEscape ) {
-
-        throw new SyntaxError(path);
-    }
-
-    if ( isChunk ) {
-        parts.push(chunk);
-    }
-
-    return parts;
-}
-
-/**
- * @private
- * @memberOf {Ctx}
- * @method
- *
- * @param {String} str
- *
- * @returns {Boolean}
- * */
-function isSpace (str) {
-
-    return R_WHITESPACE.test(str);
-}
 
 module.exports = Ctx;
