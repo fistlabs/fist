9935bd54552e3e8802ee54864b81a29b4ee77708
spacy <spacy@yandex-team.ru>
2014-12-23 08:41:08 +0300

    Do not automatically install bundled plugins

--- fist.js
+++ fist.js
@@ -13,7 +13,7 @@ var S_FIST_PLUGINS = path.join('fist_plugins', '**', '*.js');
 function fist(params) {
     var app = new Server(_.extend({}, params));
 
-    _.forEach([__dirname, app.params.root], function (dirname) {
+    _.forEach([app.params.root], function (dirname) {
         return app.install(path.join(dirname, S_FIST_PLUGINS));
     });
 
