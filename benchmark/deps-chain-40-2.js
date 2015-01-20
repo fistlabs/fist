988dd60204e9253a8794f9d11d0fb5c2cc8860c2
spacy <spacy@yandex-team.ru>
2015-01-20 13:23:09 +0300

    Refactor benchmark

---
+++ benchmark/deps-chain-40-2.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+/*eslint no-console: 0*/
+'use strict';
+
+var Benchmark = require('benchmark');
+var Suite = Benchmark.Suite;
+var buildDepsTest = require('./util/build-deps-test');
+
+Benchmark.options.minSamples = 500;
+
+console.log('Crazy deps chain, just for performance debugging');
+
+buildDepsTest(40, 2, function (run) {
+    new Suite().
+        on('cycle', function (e) {
+            console.log(String(e.target));
+        }).
+        add('test', function (defer) {
+            run(function () {
+                defer.resolve();
+            });
+        }, {
+            defer: true
+        }).
+        run({
+            async: true,
+            queued: true
+        });
+});
---
+++ benchmark/util/build-deps-test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var Core = require('../../core/core');
+var Track = require('../../core/track');
+
+var f = require('util').format;
+var uniqueId = require('unique-id');
+
+function noop() {
+    return this.name;
+}
+
+function buildDeps(unitsCount, depsPerUnit, onInit) {
+    var app = new Core({
+        logging: {
+            logLevel: 'NOTSET'
+        }
+    });
+    var deps;
+    var depsCount;
+    var unitName = f('unit_%s', unitsCount);
+
+    while (unitsCount) {
+        deps = [];
+        depsCount = Math.min(unitsCount - 1, depsPerUnit);
+
+        while (depsCount) {
+            depsCount -= 1;
+            deps[deps.length] = f('unit_%s', unitsCount - depsCount - 1);
+        }
+
+        app.unit({
+            base: 0,
+            name: f('unit_%s', unitsCount),
+            deps: deps,
+            main: noop
+        });
+        unitsCount -= 1;
+    }
+
+    app.ready().done(function () {
+        onInit(function run(done) {
+            var logger = app.logger.bind(uniqueId());
+            var track = new Track(app, logger);
+            app.callUnit(track, unitName, null, function () {
+                done();
+            });
+        });
+    });
+}
+
+module.exports = buildDeps;
