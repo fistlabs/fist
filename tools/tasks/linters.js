e5abc70479a51f11f5026cedbd8d9d36d2747345
spacy <spacy@yandex-team.ru>
2015-02-05 01:32:37 +0300

    Update gulp-one-guide

--- tools/tasks/linters.js
+++ tools/tasks/linters.js
@@ -6,6 +6,8 @@ var linterPipe = require('gulp-one-guide');
 module.exports = function () {
     this.task('lint', function () {
         return this.src(linterConf.patterns).pipe(linterPipe({
+            root: process.cwd(),
+            config: 'yandex-node',
             excludes: linterConf.excludes
         }));
     });
